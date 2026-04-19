18f1c1af03da7cb91284122b77c19a6f6836f81f
Bhawuk <bubbryofficial@gmail.com>
2026-04-19 18:12:14 +0530

    login fixed

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { supabase } from "../../lib/supabase";
-import { useState, useEffect } from "react";
+import {useState, useEffect} from "react";
 import { useRouter } from "next/navigation";
 
 
